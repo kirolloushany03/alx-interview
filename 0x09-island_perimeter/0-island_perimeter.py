11babde5d0d6e5a27928146b77f6f3ab2ca87145
root <root@fourthpyramid>
2024-09-27 21:16:30 +0300

    solving it

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,11 +2,13 @@
 """
 This module solves the island perimeter problem.
 """
+
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of an island in a grid.
 
-    Loops through the grid and checks each land cell (1). If the 
+    Loops through the grid and checks each land cell (1). If the
     neighboring cell is water (0) or out of bounds, it adds to the perimeter.
     """
     count = 0
@@ -14,15 +16,15 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
 
-                if i == 0 or grid[i-1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     count += 1
 
-                if i == len(grid) - 1 or grid[i+1][j] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     count += 1
 
-                if j == 0 or grid[i][j-1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     count += 1
 
-                if j == len(grid[i]) - 1 or grid[i][j+1] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     count += 1
     return count
